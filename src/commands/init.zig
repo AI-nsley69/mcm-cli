d58ba8a4b30b2d9a9d9e1ea5cd8ebf476fabdfcb
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2024-06-25 20:40:21 +0200

    Add version opt + check if server is initialized already

--- src/commands/init.zig
+++ src/commands/init.zig
@@ -20,26 +20,85 @@ pub const command_struct = cli.CommandT{
                 .description = "Whether to install fabric, minecraft, etc",
             }),
         },
+        cli.OptionT{
+            .name = "version",
+            .description = "Target version to install",
+            .short_name = 'v',
+            .long_name = "version",
+            .val = cli.ValueT.ofType([]const u8, .{
+                .name = "version",
+                .description = "Target version to install",
+            }),
+        },
     },
 };
 
-pub fn run(cmd: *const cli.CommandT, alloc: std.mem.Allocator) !void {
+const options = struct {
+    version: []const u8,
+    should_install: bool,
+};
+
+fn parseOptions(cmd: *const cli.CommandT) !options {
+    var should_install: bool = false;
+    var version: []const u8 = "latest";
+
     const opts = try cmd.getOpts(.{});
+
     if (opts.get("should_install")) |should_install_opt| {
         if (should_install_opt.val.isSet()) {
-            const response = try minecraft.getVersions(alloc);
-            defer response.deinit();
-
-            var released_versions = std.ArrayList(minecraft.version).init(alloc);
-            defer released_versions.deinit();
-            for (response.value.versions) |item| {
-                if (std.mem.eql(u8, item.type, "release")) {
-                    try released_versions.append(item);
-                }
-            }
-
-            const json_str = try std.json.stringifyAlloc(alloc, released_versions.items[0..5], .{ .whitespace = .indent_2 });
-            std.debug.print("Last 5 versions: {s}\n", .{json_str});
+            should_install = try should_install_opt.val.getAs(bool);
         }
+    }
+
+    if (opts.get("version")) |version_opt| {
+        if (version_opt.val.isSet()) {
+            version = try version_opt.val.getAs([]const u8);
+        }
+    }
+
+    return options{
+        .version = version,
+        .should_install = should_install,
+    };
+}
+
+fn filterForRelease(alloc: std.mem.Allocator, versions: std.ArrayList) ![]minecraft.version {
+    const list = std.ArrayList(minecraft.version).init(alloc);
+    defer list.deinit();
+
+    for (versions) |version| {
+        if (std.mem.eql(u8, version.type, "release")) {
+            try list.append(version);
+        }
+    }
+
+    return list.toOwnedSlice();
+}
+
+pub fn run(cmd: *const cli.CommandT, alloc: std.mem.Allocator) !void {
+    const writer = std.io.getStdOut().writer();
+    const is_initialized = try utils.isInitialized();
+    if (is_initialized) {
+        try writer.writeAll("This server has already been initialized.. Doing nothing\n");
+        std.process.exit(0);
+    }
+
+    const opts = try parseOptions(cmd);
+    if (opts.should_install) {
+        const response = try minecraft.getVersions(alloc);
+        defer response.deinit();
+
+        const version_requested = if (std.mem.eql(u8, opts.version, "latest")) response.value.latest.release else opts.version;
+
+        std.debug.print("Version: {s}\n", .{version_requested});
+
+        const target_version = for (response.value.versions) |version| {
+            // Only check against releases
+            if (std.mem.eql(u8, version.type, "snapshot")) continue;
+
+            if (std.mem.startsWith(u8, version.id, version_requested)) break version.id;
+        } else version_requested;
+
+        std.debug.print("Target version: {s}\n", .{target_version});
     }
 }
--- src/utils.zig
+++ src/utils.zig
@@ -1,19 +1,40 @@
-// const std = @import("std");
+const std = @import("std");
 
-// pub fn checkForConfig() !void {
-//     var path_buf: [1024]u8 = undefined;
-//     const config_dir = try std.fs.cwd().realpath(".mcm", &path_buf);
-//     const dir_exists = try std.fs.cwd().access(&path_buf, .{});
-//     if (@TypeOf(dir_exists) == void) {
-//         try std.fs.cwd().makePath(config_dir);
-//     }
+pub fn does_dir_exist(dir: std.fs.Dir, subpath: []const u8) !bool {
+    var opened = dir.openDir(subpath, .{}) catch |err| switch (err) {
+        error.FileNotFound => return false,
+        else => return err,
+    };
+    defer opened.close();
 
-//     const config_file = try std.fs.cwd().createFile(config_dir ++ "/meta.json", .{});
-//     defer config_file.close();
+    return true;
+}
 
-//     var buf: [1024]u8 = undefined;
-//     const len = try config_file.read(&buf);
-//     if (len == 0) {
-//         try config_file.writeAll("{}");
-//     }
-// }
+pub fn does_file_exist(dir: std.fs.Dir, subpath: []const u8) !bool {
+    var opened = dir.openFile(subpath, .{}) catch |err| switch (err) {
+        error.FileNotFound => return false,
+        else => return err,
+    };
+    defer opened.close();
+
+    return true;
+}
+
+pub fn isInitialized() !bool {
+    const cwd = std.fs.cwd();
+
+    const has_config_dir = try does_dir_exist(cwd, ".mcm");
+    if (!has_config_dir) {
+        return false;
+    }
+
+    var config_dir = try cwd.openDir(".mcm", .{});
+    defer config_dir.close();
+
+    const has_config_file = try does_file_exist(config_dir, "meta.json");
+    if (!has_config_file) {
+        return false;
+    }
+
+    return true;
+}
